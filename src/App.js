5487f9ec1fad03706d6bb6aef0e04cac4bc62e32
AuthenticJoonasLuoma <joonas.luoma@edu.tampere.fi>
2023-09-28 23:26:09 +0300

    lisätty karvalakkimallin vahvista tilaus -toiminto

--- src/App.js
+++ src/App.js
@@ -1,64 +1,62 @@
 import { useState, useEffect } from "react";
-import axios from 'axios';
-import NavBar from './components/NavBar';
-import ProductList from './components/ProductList';
-import ShoppingCart from './components/ShoppingCart';
-import PageFooter from './components/PageFooter';
-
+import axios from "axios";
+import NavBar from "./components/NavBar";
+import ProductList from "./components/ProductList";
+import ShoppingCart from "./components/ShoppingCart";
+import PageFooter from "./components/PageFooter";
 
 function App() {
-    const [products, setProducts] = useState([]);
+  const [products, setProducts] = useState([]);
 
-    const fetchProducts = async () => {
-        const response = await axios.get('http://localhost:3001/products');
-        setProducts(response.data);
-    }
+  const fetchProducts = async () => {
+    const response = await axios.get("http://localhost:3001/products");
+    setProducts(response.data);
+  };
 
-    const increaseAmount = async (id, newAmount) => {
-        const response = await axios.put(`http://localhost:3001/products/${id}`, {
-            ...products.find(product => product.id === id),
-            amount: newAmount,
+  const increaseAmount = async (id, newAmount) => {
+    const response = await axios.put(`http://localhost:3001/products/${id}`, {
+      ...products.find((product) => product.id === id),
+      amount: newAmount,
+    });
+
+    const updatedProducts = products.map((product) => {
+      if (product.id === id) {
+        return { ...product, ...response.data };
+      }
+
+      return product;
+    });
+
+    setProducts(updatedProducts);
+  };
+
+  const resetAmounts = async () => {
+    const updatedAmounts = products.map(async (product) => {
+      if (product.amount > 0) {
+        await axios.put(`http://localhost:3001/products/${product.id}`, {
+          ...product,
+          amount: 0,
         });
+      }
+    });
+    await Promise.all(updatedAmounts);
+    fetchProducts();
+  };
 
-        const updatedProducts = products.map((product) => {
-            if (product.id === id) {
-                return { ...product, ...response.data };
-            }
+  useEffect(() => {
+    fetchProducts();
+  }, []);
 
-            return product;
-        });
-
-        setProducts(updatedProducts);
-    }
-
-    const resetAmounts = async () => {
-        const updatedAmounts = products.map(async (product) => {
-            if (product.amount > 0) {
-                await axios.put(`http://localhost:3001/products/${product.id}`, {
-                    ...product,
-                    amount: 0,
-                });
-            }
-        });
-        await Promise.all(updatedAmounts);
-        fetchProducts();
-    };
-
-    useEffect(() => {
-        fetchProducts();
-    }, []);
-
-
-    return (
-        <div id="root">
-            <NavBar />
-            <div className="content">
-                <ProductList products={products} increaseAmount={increaseAmount} />
-                <ShoppingCart products={products} resetAmounts={resetAmounts} />
-            </div>
-            <PageFooter />
-        </div>
-    )
+  return (
+    <div id="root">
+      <NavBar />
+      <div className="content">
+        <ProductList products={products} increaseAmount={increaseAmount} />
+        <ShoppingCart products={products} resetAmounts={resetAmounts} />
+      </div>
+      <PageFooter />
+    </div>
+  );
 }
 
 export default App;
--- src/components/ShoppingCart.js
+++ src/components/ShoppingCart.js
@@ -1,48 +1,133 @@
-import { useEffect, useState } from "react";
-import ProductInCart from './ProductInCart';
-import './styles/ShoppingCartItem.css';
-
+import React, { useState, useEffect } from "react";
+import ProductInCart from "./ProductInCart";
+import "./styles/ShoppingCartItem.css";
 
 function ShoppingCart({ products, resetAmounts }) {
-    const [total, setTotal] = useState(0);
+  const [total, setTotal] = useState(0);
+  const [showModal, setShowModal] = useState(false);
+  const [orderDetails, setOrderDetails] = useState({
+    name: "",
+    email: "",
+    phone: "",
+    address: "",
+    city: "",
+    zip: "",
+  });
 
-    useEffect(() => {
-        const newTotal = products.reduce((acc, product) => {
-            if (product.amount >= 1) {
-                return acc + product.price * product.amount;
-            }
-            return acc;
-        }, 0);
+  useEffect(() => {
+    const newTotal = products.reduce((acc, product) => {
+      if (product.amount >= 1) {
+        return acc + product.price * product.amount;
+      }
+      return acc;
+    }, 0);
+    setTotal(newTotal);
+  }, [products]);
 
-        setTotal(newTotal);
+  const handlePlaceOrderClick = () => {
+    setShowModal(true);
+  };
 
-    }, [products]);
-
-    const productsInCart = products.map((product) => {
-        if (product.amount >= 1) {
-            return <ProductInCart product={product} key={product.id} />
-        }
-    });
-
-    const handleResetAmountClick = () => {
-        resetAmounts();
+  const handleConfirmOrderClick = () => {
+    if (Object.values(orderDetails).every((value) => value.trim() !== "")) {
+      alert("Kiitos tilauksestasi!\n" + JSON.stringify(orderDetails, null, 2));
+      setShowModal(false);
+      resetAmounts();
+    } else {
+      alert("Ole hyvä ja täytä kaikki kentät!");
     }
+  };
 
+  const handleInputChange = (e) => {
+    setOrderDetails({
+      ...orderDetails,
+      [e.target.name]: e.target.value,
+    });
+  };
 
+  const handleResetAmountClick = () => {
+    resetAmounts();
+  };
 
-    return (
-        <div className="cart-container">
-            <h2>Ostoskori</h2>
-            <div className="shoppingCartItem">
-                {productsInCart}
-            </div>
-            <div>{total} €</div>
-            <div>
-                <button onClick={handleResetAmountClick}>Tyhjennä Ostoskori</button>
-                <button>Vahvista tilaus</button>
-            </div>
+  const productsInCart = products
+    .map((product) => {
+      if (product.amount >= 1) {
+        return <ProductInCart key={product.id} product={product} />;
+      }
+      return null;
+    })
+    .filter(Boolean);
+
+  return (
+    <div className="cart-container">
+      <h2>Ostoskori</h2>
+      <div className="shoppingCartItem">{productsInCart}</div>
+      <div>{total} €</div>
+      <div>
+        <button onClick={handleResetAmountClick}>Tyhjennä Ostoskori</button>
+        <button onClick={handlePlaceOrderClick}>Vahvista tilaus</button>
+      </div>
+      {showModal && (
+        <div className="modal">
+          <label>
+            Nimi:{" "}
+            <input
+              type="text"
+              name="name"
+              value={orderDetails.name}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label>
+            Sähköposti:{" "}
+            <input
+              type="email"
+              name="email"
+              value={orderDetails.email}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label>
+            Puhelin:{" "}
+            <input
+              type="tel"
+              name="phone"
+              value={orderDetails.phone}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label>
+            Osoite:{" "}
+            <input
+              type="text"
+              name="address"
+              value={orderDetails.address}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label>
+            Postinumero:{" "}
+            <input
+              type="text"
+              name="zip"
+              value={orderDetails.zip}
+              onChange={handleInputChange}
+            />
+          </label>
+          <label>
+            Kaupunki:{" "}
+            <input
+              type="text"
+              name="city"
+              value={orderDetails.city}
+              onChange={handleInputChange}
+            />
+          </label>
+          <button onClick={handleConfirmOrderClick}>Vahvista</button>
         </div>
-    );
-};
+      )}
+    </div>
+  );
+}
 
 export default ShoppingCart;
